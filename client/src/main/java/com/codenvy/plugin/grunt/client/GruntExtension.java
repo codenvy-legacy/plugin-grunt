eff2549905aa5818d2fcd0d6f237d842e37a65b4
Valeriy Svydenko <vsvydenko@codenvy.com>
2015-02-06 13:52:59 +0200

    IDEX-1964: using new runner plugin

--- client/src/main/java/com/codenvy/plugin/grunt/client/GruntExtension.java
+++ client/src/main/java/com/codenvy/plugin/grunt/client/GruntExtension.java
@@ -14,7 +14,6 @@ import com.codenvy.ide.api.action.ActionManager;
 import com.codenvy.ide.api.action.DefaultActionGroup;
 import com.codenvy.ide.api.constraints.Constraints;
 import com.codenvy.ide.api.extension.Extension;
-import com.codenvy.ide.extension.runner.client.RunnerLocalizationConstant;
 import com.codenvy.plugin.grunt.client.menu.CustomGruntRunAction;
 import com.codenvy.plugin.grunt.client.menu.LocalizationConstant;
 import com.google.inject.Inject;
@@ -22,6 +21,7 @@ import com.google.inject.Singleton;
 
 import static com.codenvy.ide.api.action.IdeActions.GROUP_RUN;
 import static com.codenvy.ide.api.constraints.Anchor.AFTER;
+import static com.codenvy.ide.ext.runner.client.constants.ActionId.CUSTOM_RUN_APP;
 
 
 /**
@@ -35,7 +35,6 @@ public class GruntExtension {
     @Inject
     public GruntExtension(ActionManager actionManager,
                           LocalizationConstant localizationConstant,
-                          RunnerLocalizationConstant runnerLocalizationConstants,
                           CustomGruntRunAction customGruntRunAction) {
 
         actionManager.registerAction(localizationConstant.gruntCustomRunId(), customGruntRunAction);
@@ -44,7 +43,7 @@ public class GruntExtension {
         DefaultActionGroup runMenuActionGroup = (DefaultActionGroup)actionManager.getAction(GROUP_RUN);
 
         // create constraint
-        Constraints afterBuildConstraints = new Constraints(AFTER, runnerLocalizationConstants.customRunAppActionId());
+        Constraints afterBuildConstraints = new Constraints(AFTER, CUSTOM_RUN_APP.getId());
 
         // Add Custom Grunt Run in build menu
         runMenuActionGroup.add(customGruntRunAction, afterBuildConstraints);
